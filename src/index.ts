9b624f038a3c0482672fd576c8f57adbe31b2be6
Adrian Jasaroska <adrianjasaroska@gmail.com>
2025-03-06 13:45:25 +0100

    Ab06 Aufagbe 2f

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,12 @@ app.get('/', (req: Request, res: Response) => {
   res.send(`<h1>Hello World!</h1><p>Host: ${os.hostname()}</p>`);
 });
 
+app.get("/log/time", (req: Request, res: Response) => {
+  const currentTime = new Date().toISOString();
+  console.log(`[LOG] Current Time: ${currentTime}`);
+  res.json({ message: "Time logged!", time: currentTime });
+});
+
 const serverPort = process.env.PORT || 3000;
 server.listen(serverPort, () => {
   // eslint-disable-next-line no-console
